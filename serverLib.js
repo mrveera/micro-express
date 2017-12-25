287f4ac6922a97d113776e331c3dd2666bad9b62
veera83372 <veeravenky222@gmail.com>
2017-12-25 11:34:42 +0530

    File server separated from server.js
    -server lib contains it's common functionalities like respond,
    -request ,response logger and it also contains handler

---
+++ serverLib.js
@@ -0,0 +1,27 @@
+const lib={};
+
+lib.handlers = {
+  "POST": {},
+  "GET": {}
+}
+
+lib.logRequest = function() {
+  let date = new Date().toLocaleString()
+  console.log(`method: ${this.method } url: ${this.url}  Date: ${date} `);
+}
+
+lib.logResponse = function() {
+  console.log(`statusCode: ${this.statusCode} for:${this.for}`);
+  console.log(`---------------`)
+}
+
+lib.respond = function (res,content,statusCode,header,encoding) {
+  let headerKeys = Object.keys(header);
+  headerKeys.forEach(function (key) {
+    res.setHeader(key,header[key]);
+  });
+  res.statusCode=statusCode;
+  res.write(content,encoding);
+  lib.logResponse.call(res);
+  res.end();
+}
